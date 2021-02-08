fe9d1d3cb756fe934bd5c2515302809fb1ef2389
Darina000 <d.zeml0137@gmail.com>
2021-02-08 17:04:12 +0300

    commit 1 in NewBranch

--- Week1/main.cpp
+++ Week1/main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "World!\n";
     std::cout << "Hello" << std::endl;
     std::cout << "New" << std::endl;
     
