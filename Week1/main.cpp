163385f6809cd86f0beba85f016cfaa6eabf5834
Darina000 <d.zeml0137@gmail.com>
2021-02-08 17:09:48 +0300

    commit 1 in master(step 2 in task 2)

--- Week1/main.cpp
+++ Week1/main.cpp
@@ -7,7 +7,7 @@ int main() {
     std::cout << "World!\n";
     std::cout << "Hello" << std::endl;
     std::cout << "New" << std::endl;
-    
+    int w = 3;
     int a = 4;
     int c = 6;
     return 0;
