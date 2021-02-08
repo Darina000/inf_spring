d34538b5a1eaf42dda9f1b08fd588328dc28cc22
Darina000 <d.zeml0137@gmail.com>
2021-02-08 16:53:55 +0300

    second changed

--- Week1/main.cpp
+++ Week1/main.cpp
@@ -5,5 +5,7 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    std::cout << "Hello" << std::endl;
+    std::cout << "New" << std::endl;
     return 0;
 }
