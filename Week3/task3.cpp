76f92c1e7be05e1fe7cafd189237cfb12284b967
Darina000 <d.zeml0137@gmail.com>
2021-03-09 12:04:24 +0300

    week3, task3

--- Week3/task3.cpp
+++ Week3/task3.cpp
@@ -2,63 +2,75 @@
 #include <functional>
 #include <boost/functional/hash.hpp>
 #include <string>
-
+#include <unordered_set>
 #include <chrono>
 #include <random>
 #include <set>
 #include <stdlib.h>
 
-struct Struct{
-    int a;
-};
+#include <boost/container_hash/hash.hpp>
+#include  <boost/type_index/type_index_facade.hpp>
 
 
 std::set < std::string > make_random_words(std::size_t N, std::size_t length) // length = 10 - good enough
 {
-    std::uniform_int_distribution <> letter(97, 122);
-    std::default_random_engine e(static_cast < std::size_t > (
-        std::chrono::system_clock::now().time_since_epoch().count()));
+   std::uniform_int_distribution <> letter(97, 122);
+   std::default_random_engine e(static_cast < std::size_t > (
+       std::chrono::system_clock::now().time_since_epoch().count()));
 
-    std::set < std::string > words;
+   std::set < std::string > words;
 
-    for (std::string s(length, '_'); std::size(words) < N; words.insert(s))
-        for (auto & c : s)
-            c = letter(e);
+   for (std::string s(length, '_'); std::size(words) < N; words.insert(s))
+       for (auto & c : s)
+           c = letter(e);
 
-    return words;
+   return words;
 }
 
+template < typename T >
+void hash_combine(std::size_t & seed, const T & value) noexcept
+{
+   seed ^= std::hash < T > ()(value) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
 
+template < typename T >
+void hash_value(std::size_t & seed, const T & value) noexcept
+{
+   hash_combine(seed, value);
+}
 
+template < typename T, typename ... Types >
+void hash_value(std::size_t & seed, const T & value, const Types & ... args) noexcept
+{
+   hash_combine(seed, value);
+   hash_value(seed, args...);
+}
+
+template < typename ... Types >
+std::size_t hash_value(const Types & ... args) noexcept
+{
+   std::size_t seed = 0;
+   hash_value(seed, args...);
+   return seed;
+}
 
 int main(int argc, char ** argv)
 {
-    std::set <std::size_t> Key;
-    
-    Struct one;
-    
-    int n = 2000;
-    
-        int Colis = 0;
-    
-        for (int i = 0; i<n; ++i){
-            one.a = rand()%n;
-            std::size_t N = 10;
-            std::size_t length =  10;
-            std::set < std::string > words =  make_random_words(N, length);
-        
-            if ((!Key.insert(boost::hash_value(one.a)).second)){
-                ++Colis;
-            }
-            for (auto j: words){
-                if(!Key.insert(boost::hash_value(j)).second){
-                    ++Colis;
-                }
-            }
-            
-        }
-    
-    std::cout << n << " " << Colis << std::endl;
-    return 0;
+   std::set <std::size_t> Key;
+   int n = 10000;
+   const std::size_t length =  10;
+   const std::size_t N = 10000;
+   std::set < std::string > words =  make_random_words(N, length);
+   int Colis = 0;
+   
+   for (int i = 0; i< n; ++i){
+   for (auto j: words){
+       int a = rand();
+       if(!Key.insert(hash_value(j, a)).second){
+           ++Colis;
+       }
+   }}
+   
+   std::cout << n << " " << Colis << std::endl;
+   return 0;
 }
-
