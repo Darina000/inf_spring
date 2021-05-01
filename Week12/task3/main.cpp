88db9349e5a403965e52296475fa6ae177fc68bf
Darina000 <61585500+Darina000@users.noreply.github.com>
2021-05-01 08:14:01 +0300

    Update main.cpp

--- Week12/task3/main.cpp
+++ Week12/task3/main.cpp
@@ -6,8 +6,8 @@
 class Particle
 {
 public:
-    Particle(const sf::Vector2f position, const float radius, const sf::Vector2f speed) :
-        m_position(position), m_radius(radius), m_speed(speed)
+    Particle(sf::Vector2f position, const float radius, const sf::Vector2f speed) :
+        m_position(position), m_radius(radius), m_speed(speed), disx(- m_speed.x, m_speed.x),  disy(- m_speed.y, m_speed.y)
     {}
 public:
     void draw(sf::RenderWindow &m_window) {
@@ -17,30 +17,30 @@ public:
         m_window.draw(circle);
     }
     void move(const unsigned int W, const unsigned int H, std::mt19937 gen) {
-        std::uniform_real_distribution<float> disx(- m_speed.x, m_speed.x);
-        std::uniform_real_distribution<float> disy(- m_speed.y, m_speed.y);
         m_position.x += disx(gen);
         m_position.y += disy(gen);
         if (m_position.x > W) m_position.x = 0.f; if (m_position.x < 0) m_position.x = static_cast<float>(W);
         if (m_position.y > H) m_position.y = 0.f; if (m_position.y < 0) m_position.y = static_cast<float>(H);
     }
-    sf::Vector2f getPosition() {
+    sf::Vector2f getPosition() noexcept  {
         return m_position;
     }
-    sf::Vector2f getSpeed() {
+    sf::Vector2f getSpeed() noexcept {
         return m_speed;
     }
 private:
     sf::Vector2f m_position;
     float m_radius;
     sf::Vector2f m_speed;
+    std::uniform_real_distribution<float> disx;
+    std::uniform_real_distribution<float> disy;
 
 };
 
 class Visualizer
 {
 public:
-    Visualizer(int count, float width, float height): m_count(count), m_width(width), m_height(height)
+    Visualizer(int count, float width, float height): m_count(count), m_width(width), m_height(height), step(width / 20.0f), speed(10.f, 10.f)
     {
     }
     bool isInside(Particle p, float xPos, float yPos, float step) {
@@ -64,46 +64,50 @@ public:
         }
     }
     int run() {
-        const auto step = m_width / 20.0f;
-        sf::RenderWindow window{ sf::VideoMode(static_cast<const unsigned int>(m_width),static_cast<const unsigned int> (m_height)), "PHYSICS" };
-        window.setFramerateLimit(40);
+           sf::RenderWindow window{ sf::VideoMode(static_cast<const unsigned int>(m_width),static_cast<const unsigned int> (m_height)), "PHYSICS" };
+           window.setFramerateLimit(40);
+        
+           std::mt19937 gen(rd());
+           std::uniform_real_distribution<float> dis(0, 600);
+           
+           for (int i = 0; i < m_count; i++)
+           {
+               sf::Vector2f position(dis(gen), dis(gen));
+               particles.push_back(Particle(position, 5.0, speed));
+           }
+           while (window.isOpen())
+           {
+               sf::Event event;
+               while (window.pollEvent(event))
+               {
+                   if (event.type == sf::Event::Closed)
+                   {
+                       window.close();
+                   }
+               }
+               window.clear();
+               paintSquare(step, m_width, m_height, window, particles);
+               for (auto i = 0U; i < static_cast<unsigned int>(m_count); ++i) {
+                   std::random_device rd;
+                   std::mt19937 genm(rd());
+                   particles[i].move(600U, 600U, genm);
+                   particles[i].draw(window);
+               }
+               window.display();
+           }
+           return EXIT_SUCCESS;
+       }
+   private:
+       int m_count;
+       const float m_width ;
+       const float m_height ;
         std::vector < Particle > particles;
         std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_real_distribution<float> dis(0, 600);
-        sf::Vector2f speed(10.f, 10.f);
-        for (int i = 0; i < m_count; i++)
-        {
-            sf::Vector2f position(dis(gen), dis(gen));
-            particles.push_back(Particle(position, 5.0, speed));
-        }
-        while (window.isOpen())
-        {
-            sf::Event event;
-            while (window.pollEvent(event))
-            {
-                if (event.type == sf::Event::Closed)
-                {
-                    window.close();
-                }
-            }
-            window.clear();
-            paintSquare(step, m_width, m_height, window, particles);
-            for (auto i = 0U; i < static_cast<unsigned int>(m_count); ++i) {
-                std::random_device rd;
-                std::mt19937 genm(rd());
-                particles[i].move(600U, 600U, genm);
-                particles[i].draw(window);
-            }
-            window.display();
-        }
-        return EXIT_SUCCESS;
-    }
-private:
-    int m_count;
-    const float m_width ;
-    const float m_height ;
-};
+        const float step;
+        sf::Vector2f speed;
+   };
+
+
 int main() {
     Visualizer myV(20, 600.f, 600.f);
     myV.run();
